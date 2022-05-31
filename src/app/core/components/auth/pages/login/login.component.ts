7558a2f63626282ee97064eb093bd96ef1b4c75d
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-05-31 16:55:56 -0400

    UI da tela de login implementada

---
+++ src/app/core/components/auth/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public formAuth: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  public msgError!: string;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  public submitForm() {}
+}
